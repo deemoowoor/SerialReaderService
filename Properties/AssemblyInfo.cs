3805cd5e4ea3a581fc5636be441429c717ff39ee
Andrei Sosnin <andrei.sosnin@gmail.com>
2011-05-15 00:23:17 +0200

    * added missing file

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Serial Speed Converter")]
+[assembly: AssemblyDescription("Serial to TCP bicycle wheel speed controller")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Andrei Sosnin")]
+[assembly: AssemblyProduct("SerialSpeedConverter.Properties")]
+[assembly: AssemblyCopyright("(c) 2011 Andrei Sosnin")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8458d35f-3976-4854-87bc-d4df86e83007")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
